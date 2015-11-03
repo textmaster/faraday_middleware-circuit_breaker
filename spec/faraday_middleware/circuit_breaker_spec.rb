35d66cbd65dc18f4e2a8827dfb4a9973032411fb
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2015-11-03 15:17:33 +0100

    add spec

--- spec/faraday_middleware/circuit_breaker_spec.rb
+++ spec/faraday_middleware/circuit_breaker_spec.rb
@@ -1,11 +1,33 @@
-require 'spec_helper'
-
 describe FaradayMiddleware::CircuitBreaker do
-  it 'has a version number' do
-    expect(FaradayMiddleware::CircuitBreaker::VERSION).not_to be nil
+
+  def connection(options = {})
+    Faraday.new do |c|
+      c.use :circuit_breaker, options
+      c.adapter :test do |stub|
+        stub.get('/success') do
+          [200, {}, '']
+        end
+        stub.get('/failure') do
+          [500, {}, '']
+        end
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it { expect(connection.get('/success').status).to eq(200) }
+  it { expect(connection.get('/failure').status).to eq(500) }
+  it { expect(connection.get('/blank').status).to eq(503) }
+
+  describe 'on failure' do
+
+    let(:fallback) { double }
+    let(:response) { Faraday::Response.new(status: 503, response_headers: {}) }
+
+    it 'calls fallback' do
+      expect(fallback).to receive(:foo).and_return(response)
+      expect(connection(fallback: fallback.method(:foo)).get('/blank').status).to eq(503)
+    end
+
   end
+
 end
